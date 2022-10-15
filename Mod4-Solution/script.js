e11c2d38a9b59d36791deebd396cb48571046ab9
Ritesh Tiwari <84981640+RiteshTiwari554@users.noreply.github.com>
2022-10-15 11:14:07 +0530

    Update script.js

--- Mod4-Solution/script.js
+++ Mod4-Solution/script.js
@@ -11,8 +11,10 @@ names[8]="laura";
 names[9]="jim";
 
 
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+for (var i = 0; i < names.length; i++) 
+{
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j')
+	{
         console.log("Goodbye "+ names[i])
 	}
 	else{
